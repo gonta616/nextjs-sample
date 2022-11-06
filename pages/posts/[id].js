4a7c73d2ca34d4a66ebb5a3ebb4b466e83906d67
kgonjo <gongongonta616@gmail.com>
2022-11-06 21:35:35 +0900

    Next.js init

---
+++ pages/posts/[id].js
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+
+// next build 時に [id]の部分を決める
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// next build時にPostに渡すパラメータ
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <Date dateString={postData.date} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
